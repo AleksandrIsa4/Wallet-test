365ada7ee73f58bd8c5eeca7692cb37473d1806b
AleksandrIsa4 <isachenkov.am@yandex.ru>
2024-05-14 23:32:40 +0300

    Изменение WalletService

--- src/main/java/org/example/controller/WalletController.java
+++ src/main/java/org/example/controller/WalletController.java
@@ -5,8 +5,10 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.dto.WalletResponse;
 import org.example.entity.Wallet;
-import org.example.model.WalletDto;
+import org.example.dto.WalletDto;
+import org.example.mapper.WalletMapper;
 import org.example.service.WalletService;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.*;
@@ -19,23 +21,26 @@ public class WalletController {
 
     private final WalletService walletService;
 
+    private final WalletMapper walletMapper;
+
     @Operation(summary = "Изменение баланса кошелька")
     @PostMapping(value = "/wallet")
-    @Async
-    public Wallet post(@RequestBody @Valid @NotNull WalletDto walletDto) {
-        return walletService.post(walletDto);
+    public WalletResponse post(@RequestBody @Valid @NotNull WalletDto walletDto) {
+        Wallet wallet=walletService.post(walletDto);
+        return walletMapper.toWalletResponseFromWallet(wallet);
     }
 
     @Operation(summary = "Получить баланс кошелька")
     @GetMapping(value = "wallets/{WALLET_UUID}")
-    @Async
-    public Wallet get(@PathVariable Long WALLET_UUID) {
-        return walletService.get(WALLET_UUID);
+    public WalletResponse get(@PathVariable Long WALLET_UUID) {
+        Wallet wallet=walletService.get(WALLET_UUID);
+        return walletMapper.toWalletResponseFromWallet(wallet);
     }
 
     @Operation(summary = "Создание кошелька")
     @PostMapping(value = "/wallet/create")
-    public Wallet createWallet() {
-        return walletService.create();
+    public WalletResponse createWallet() {
+        Wallet wallet=walletService.create();
+        return walletMapper.toWalletResponseFromWallet(wallet);
     }
 }
